3315eee17773c5ddf48b6053ac91df03d386f6d0
alexkarumbi <alexkarumbimuhia@gmail.com>
2024-10-25 12:48:13 +0300

    Added popup.js code

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.getElementById("fetch").addEventListener("click", () => {
+    const service = document.getElementById("service").value;
+    chrome.runtime.sendMessage({ action: "fetchCredentials", service: service }, (response) => {
+      if (response.success) {
+        document.getElementById("result").innerText = `Username: ${response.data.username}, Password: ${response.data.password}`;
+      } else {
+        document.getElementById("result").innerText = `Error: ${response.error}`;
+      }
+    });
+  });
