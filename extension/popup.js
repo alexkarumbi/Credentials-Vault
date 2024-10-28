ff26f0646bfa9617990a6907b3b7cc54d92a194f
alexkarumbi <alexkarumbimuhia@gmail.com>
2024-10-28 17:29:45 +0300

    Added copy functionality for password

--- extension/popup.js
+++ extension/popup.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const service = document.getElementById('service').value;
     chrome.runtime.sendMessage({ action: 'fetchCredentials', service: service }, function(response) {
       if (response.success) {
+        document.getElementById('username').value = response.data.username;
         document.getElementById('password').value = response.data.password;
       } else {
+        document.getElementById('username').value = '';
         document.getElementById('password').value = `Error: ${response.error}`;
       }
     });
@@ -12,8 +14,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.getElementById('copy').addEventListener('click', function() {
     const passwordField = document.getElementById('password');
+    passwordField.type = 'text'; // Temporarily change the type to text to copy the value
     passwordField.select();
     document.execCommand('copy');
+    passwordField.type = 'password'; // Change the type back to password
     alert('Password copied to clipboard');
   });
 });
